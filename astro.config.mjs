18f8b7042ddd62cda0dc1430ba6370ae9384f49a
Furkan Demir <frndemir@gmail.com>
2022-12-11 00:09:49 +0300

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jsizmir.github.io",
+  integrations: [tailwind()],
 });
