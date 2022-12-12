afaf7a15ecca2399e2026506e35aecd741ccc79c
Furkan Demir <furkandemir@192.168.1.100>
2022-12-13 00:21:14 +0300

    update theme and content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: "Inter, sans-serif",
+      colors: {
+        umbra: "#201F1F",
+        primary: "#f0db4f",
+      },
+    },
+  },
+  plugins: [],
+};
