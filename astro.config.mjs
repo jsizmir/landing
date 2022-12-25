f5cd65b947af410ba0df7db9281707281f1f907f
fdemir <frndemir@gmail.com>
2022-12-26 01:47:19 +0300

    chore: change basePath to base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jsizmir.github.io",
-  basePath: "/landing",
+  base: "/landing",
   integrations: [tailwind()],
 });
